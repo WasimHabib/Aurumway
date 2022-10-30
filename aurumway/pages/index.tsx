b7bcc6b2307e5f8677c8495fd11b5d4b73546426
Zohaib Wasim <zohaib.wasim@gmail.com>
2022-10-30 11:35:15 -0400

    added scroll snapping for main page.

--- aurumway/pages/index.tsx
+++ aurumway/pages/index.tsx
@@ -1,6 +1,8 @@
 import { gsap } from "gsap"
+import { useLayoutEffect } from "react";
 import { Button } from "../lib/atoms/Button";
 import InfoBox from "../lib/components/InfoBox";
+import { colors } from "../lib/models/contants";
 
 
 function Home() {
@@ -9,17 +11,54 @@ function Home() {
     console.log("clicked button");
   }
 
+  useLayoutEffect(() => {
+      
+  }, []);
+
   return (
     <>
-      <div >
+    <div style={BackDropStyles} className='container' >
+        <div style={EntranceStyles} className='scroll-area'>1</div>
+        <div style={MainStyles} className='scroll-area'>2</div>
+        <div style={ContactStyles} className='scroll-area'>3</div>
+    </div>
+
+      {/* <div >
           <Button type="SUCCESS" id="MyButton" text="Click Me" onClick={clickFunc} width="100px" height="50px" className="Button" />
           <div style={{margin: 'auto', maxWidth: '500px'}}>
             <InfoBox  width="500px" height="500px" hiddenHeight="150px" hiddenWidth="300px" hasFooter={true} direction="RIGHT" bodytext={text}/>
           </div>
        
-      </div>
+      </div> */}
     </>
   );
+}
+
+var EntranceStyles: React.CSSProperties = {
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: 'green',
+    scrollSnapAlign: 'start'
+}
+
+var MainStyles = {
+  width: '100vw',
+  height: '100vh',
+  backgroundColor: 'blue',
+  scrollSnapAlign: 'center'
+}
+
+var ContactStyles = {
+  width: '100%',
+  height: '100vh',
+  backgroundColor: 'red',
+  scrollSnapAlign: 'start'
+}
+
+var BackDropStyles = {
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: colors.colorPrimary
 }
 
 export default Home
