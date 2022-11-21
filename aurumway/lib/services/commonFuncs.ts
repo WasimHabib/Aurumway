bd93431cd62365e5282846c9dc72d4d2a84c6987
Zohaib Wasim <zohaib.wasim@gmail.com>
2022-11-21 08:24:35 -0500

    optimization updates 2

--- aurumway/lib/services/commonFuncs.ts
+++ aurumway/lib/services/commonFuncs.ts
@@ -1,23 +1,33 @@
+import { send } from "@emailjs/browser";
+
 //submit the form element
 export function onSubmit(event:any) {
 
 }
 
 export function sendEmail(firstName: string, lastName: string, email: string, message: string): Promise<any> {
-    let result = true;
-    let bod = {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        message: message
-    }
-    return fetch('https://us-central1-aurumway-c85f5.cloudfunctions.net/AurumwayAPI/sendEmail', {
-        method: 'POST',
-        cache: 'no-cache',
-        body: JSON.stringify(bod),
-        headers: {
-            'Content-Type': 'application/json'
-          },
-
-    })
+    let result = new Promise<any>((resolve, reject) => {
+            fetch("https://us-central1-aurumway-c85f5.cloudfunctions.net/AurumwayAPI/getAPIKey")
+            .then(response => {
+                response.text().then(val => {
+                    send('contact_service', 'contact_form', {
+                            from_name: firstName + " " + lastName,
+                            from_email: email,
+                            message: message
+                        }, val).then((res) => {
+                            resolve(res);
+                        })
+                        .catch((err) => {
+                            reject(err);
+                        })
+                })
+                .catch((err) => {
+                    reject(err);
+                })
+            })
+            .catch((err) => {
+                reject(err);
+            })
+    });
+    return result;
 }
